88b26d0b5d0c450fb9080c91679900f1c965080c
Anish George <Anish_George-GGN@external.mckinsey.com>
2019-01-22 11:22:27 +0530

    IDEA testing setup correctly

---
+++ test/src/com/anishgeorge/tddfractions/SetupTest.java
@@ -0,0 +1,12 @@
+package com.anishgeorge.tddfractions;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class SetupTest {
+
+    @Test
+    public void isThisWorking() {
+        Assert.assertTrue(true);
+    }
+}
