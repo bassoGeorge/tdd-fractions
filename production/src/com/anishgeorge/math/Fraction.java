ffb66896e9ff2dd74a34a51cc40d5896585d6e9a
Anish George <anishgeo@thoughtworks.com>
2019-01-22 21:59:06 +0530

    Get some basic whole number additions in

---
+++ production/src/com/anishgeorge/math/Fraction.java
@@ -0,0 +1,17 @@
+package com.anishgeorge.math;
+
+public class Fraction {
+    private int integerValue;
+
+    public Fraction(int integerValue) {
+        this.integerValue = integerValue;
+    }
+
+    public Fraction plus(Fraction that) {
+        return this;
+    }
+
+    public int intValue() {
+        return integerValue;
+    }
+}
---
+++ test/src/com/anishgeorge/math/test/AddFractionTest.java
@@ -0,0 +1,21 @@
+package com.anishgeorge.math.test;
+
+import com.anishgeorge.math.Fraction;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class AddFractionTest {
+
+    @Test
+    public void zeroPlusZero() {
+        Fraction sum = new Fraction(0).plus(new Fraction(0));
+        assertEquals(0, sum.intValue()); // intValue is scaffolding
+    }
+
+    @Test
+    public void nonZeroPlusZero() {
+        Fraction sum = new Fraction(1).plus(new Fraction(0));
+        assertEquals(1, sum.intValue());
+    }
+}
